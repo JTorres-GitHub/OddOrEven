9c267554e57bf1367e395cb02338f744b1540b93
Joaquin.Torres <torres.joaquin@gmail.com>
2020-06-12 13:40:17 -0400

    Changed odd/even check to function.

--- OddOrEven/src/OddOrEven.java
+++ OddOrEven/src/OddOrEven.java
@@ -1,18 +1,30 @@
 import java.util.Scanner;
 
 public class OddOrEven {
+	
+	static String OddEven(int num){
+		String flag;
+		
+		if (num % 2 == 0)
+			flag = "even";
+		else
+			flag = "odd";	
+		
+		return flag;
+	}
+	
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
 		int num;
+		String result;
 		
 		System.out.println("Enter an integer: ");
 		
 		num = input.nextInt();
 		
-		if (num % 2 == 0)
-			System.out.println(num + " is even.");
-		else
-			System.out.println(num + " is odd.");
+		result = OddEven(num);
+		
+		System.out.println(num + " is " + result + ".");
 		
 		input.close();
 	}
